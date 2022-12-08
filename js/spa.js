1f190047a25ce36704aaa1bd67e26daf2a0e2895
diegotrevisancc <diego.trevisan.cc@gmail.com>
2022-12-08 15:48:42 -0300

    finish

---
+++ js/spa.js
@@ -0,0 +1,33 @@
+const section = document.querySelector(".software");
+const botao = document.querySelector(".button_spa");
+const title = document.querySelector(".spa_title");
+const desc = document.querySelector(".spa_description");
+let page = "inicial";
+
+const spaImg = document.querySelector(".old-img");
+
+const botaoOldHtml = botao.innerHTML;
+const titleOldHtml = title.innerHTML;
+const descOldHtml = desc.innerHTML;
+const spaImgOldSrc = spaImg.getAttribute("src");
+
+botao.addEventListener("click", () => {
+  if (page === "inicial") {
+    spaImg.classList.add(".spa-img");
+    spaImg.setAttribute("src", "images/atitus.jpg");
+    section.classList.add("software-spa");
+    title.innerHTML = `Uma SPA funciona dessa forma (=`;
+    desc.innerHTML = `Olha só funcionou!`;
+    botao.classList.add("button_spa_clicked");
+    botao.innerHTML = "Voltar";
+    page = "spa";
+  } else if (page === "spa") {
+    spaImg.setAttribute("src", spaImgOldSrc);
+    section.classList.remove("software-spa");
+    title.innerHTML = titleOldHtml;
+    desc.innerHTML = descOldHtml;
+    botao.classList.remove("button_spa_clicked");
+    botao.innerHTML = botaoOldHtml;
+    page = "inicial";
+  }
+});
